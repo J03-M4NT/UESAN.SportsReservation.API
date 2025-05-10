1bead249b8da7f56e2a990c0bf8ae12e737af374
J03_M4NT <jmantillah1503@gmail.com>
2025-05-09 21:11:26 -0500

    Arreglo ultimos

--- UESAN.SportsReservation.API/Program.cs
+++ UESAN.SportsReservation.API/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using UESAN.SportsReservation.API.Infrastructure.Data;
+using UESAN.SportsReservation.CORE.Core.Interfaces;
+using UESAN.SportsReservation.CORE.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var _configuration = builder.Configuration;
+
+var connectionString = _configuration
+                        .GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<SportsReservationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+// TODO: Add Interfaces
+builder.Services.AddScoped<ICanchaRepository, CanchasRepository>();
+
+
+
+// - - - - - - - - 
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
